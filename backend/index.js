bff410b571d413d9af378a608dded9a9dd09ca1c
parshva555 <parshvadani@gmail.com>
2023-10-23 12:51:53 +0530

    connection to mongodb and updating the model user

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const connectToMongo = require('./db');
+connectToMongo();
 const app = express();
+const port = 5000;
+app.use(express.json())
 
-const port = 3000;
-
-app.listen(port,() => {
-    console.log(`listening at port ${port}`)
-})
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+  })
